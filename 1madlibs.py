90053d85f8e280d1d8d12b41ab234e9480256962
Mohammad Aakash <64223049+MuhammadAakash@users.noreply.github.com>
2021-04-18 10:38:50 +0500

    Add files via upload
    First Beginner Project in Python

---
+++ 1madlibs.py
@@ -0,0 +1,16 @@
+noun1 = input("Noun: ")
+verb1 = input("Verb: ")
+adject1 = input("Adjective: ")
+noun2 = input("Noun: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+verb4 = input("Verb: ")
+adject2 = input("Adjective: ")
+
+madlibs = f"Once an old man and a {noun1} were {verb1} on the shore of a wild and {adject1}.\
+    Old man said {noun2} is the indefinite continued progress of existence and events that \
+    occur in an apparently irreversible succession from the {verb2}, through the {verb3},\
+    into the {verb4}.\
+    It is a component quantity of various {adject2}."
+
+print(madlibs)
